7ecb4dd37d5808beaaeba6200d54d276fa94764c
moritz <rosenthal.moritz@icloud.com>
2024-10-24 14:01:07 +0200

    Era1 und Era1.c datei geändert

--- Era1.c
+++ Era1.c
@@ -4,6 +4,7 @@ int main(void) {
 	int c=0;
 	for(int i=0; i<10; i++){
 		c=c+i;
+		printf("Der aktuelle Wert von c ist: %i\n", c);
 	}
-	printf("Der Wert von c ist: %i\n", c);
+	printf("\nDer endgültige Wert von c ist: %i\n", c);
 }
